50c9f668e69a3c7ec96c44884698ce0637892560
RaquelLima7 <raquellimadev@gmail.com>
2022-02-28 18:35:53 -0300

    class 6

---
+++ Class_6/class6.py
@@ -0,0 +1,16 @@
+# Variáveis
+
+nome = 'Raquel Limâ'
+idade = 29
+altura = 1.65
+e_maior = idade > 18
+peso = 40
+imc = peso / (altura * altura)
+
+print(nome, 'tem', idade, 'anos de idade e seu imc é', imc)
+
+# print('Nome:', nome)
+# print('Idade:', idade)
+# print('Altura:', altura)
+# print('É maior:', e_maior)
+
