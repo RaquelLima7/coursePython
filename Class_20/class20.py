8564050daaf4100381861ad2cc8c69df96bb1afc
RaquelLima7 <raquellimadev@gmail.com>
2022-03-03 13:54:08 -0300

    class 20

---
+++ Class_20/class20.py
@@ -0,0 +1,23 @@
+# Iterando strings com while em Python
+
+frase = 'o rato roeu a roupa do rei de roma'
+tamanho_frase = len(frase)
+contador = 0
+nova_string = ''
+
+# while contador < tamanho_frase:
+#     # print(frase[contador], contador)
+#     nova_string += frase[contador]
+#     contador += 1
+#     print(nova_string)
+
+while contador < tamanho_frase:
+    letra = frase[contador]
+    if letra == 'r':
+        nova_string += 'R'
+    else:
+        nova_string += letra
+    contador += 1
+
+print(nova_string)
+
